1342560c201b9a736b665065edcbb392f373022a
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2022-11-02 09:38:32 +0100

    verbetering text + style component verplaatsen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image,} from 'react-native';
 import BookTile from './components/bookTile'
 
 export default function App() {
@@ -15,10 +15,10 @@ export default function App() {
           <Text style={styles.shopCount}>1</Text>
         </View>
       </View>
-      <BookTile title="Book title 1" author="by Som Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-8-2165-1860-1"/>
-      <BookTile title="Book title 2" author="by Som Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-2-1425-4277-8"/>
-      <BookTile title="Book title 3" author="by Som Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-5-9684-0436-7"/>
-      <BookTile title="Book title 4" author="by Som Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-8-3356-4834-0"/>
+      <BookTile title="Book title 1" author="by Some Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-8-2165-1860-1"/>
+      <BookTile title="Book title 2" author="by Some Random dude" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-2-1425-4277-8"/>
+      <BookTile title="Book title 3" author="by Jane Doe" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-5-9684-0436-7"/>
+      <BookTile title="Book title 4" author="by Jane Doe" discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interger luctus, nulla sit amet pellentesque ultricies, mangna nisl vulputate urna, quis feugiat purus emin id ex." isbn="ISBN 978-8-3356-4834-0"/>
 
       <StatusBar style="auto" />
     </View>
