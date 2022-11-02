f023db8d2f1e7103aca691ca052561867b30ab9e
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2022-11-02 09:21:17 +0100

    book component

---
+++ components/bookTile.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+const bookTile = (props) => {
+    return  (
+        <View style={styles.bookTile}>
+        <Text style={styles.bookTitle}>{props.title}</Text>
+        <Text>{props.author}</Text>
+        <View style={styles.bookDescription}>
+          <Text>{props.discription}</Text>
+        </View>
+        <View style={styles.bookDetail}>
+          <Text>{props.isbn}</Text>
+          <Text style={styles.button}>READ</Text>
+        </View>
+      </View>
+    );
+ }
+
+ const styles = StyleSheet.create({
+    bookTitle:{
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    bookDescription:{
+      marginVertical: '2%',
+  
+    },
+    bookDetail:{
+      display: 'flex',
+      marginVertical: '2%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    button:{
+      paddingHorizontal: '2%',
+      paddingVertical: '1%',
+      borderColor: '#000',
+      borderWidth: 1,
+      borderStyle: 'solid',
+    }
+  });
+  
+
+
+
+ export default bookTile;
