f76f8c75deee2a2fba9a27384458e68c3a495fd2
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2022-11-02 08:59:39 +0100

    title + shopping bag

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.head}>
+        <Text style={styles.header}>Bookshop</Text>
+        <View style={styles.shop}>  
+        <Image
+          style={styles.icon}
+          source={require('./assets/shoppingCart.png')}
+        />
+          <Text style={styles.shopCount}>1</Text>
+        </View>
+      </View>
       <View style={styles.bookTile}>
         <Text style={styles.bookTitle}>Book title 1</Text>
         <Text>By Some Random Dude</Text>
@@ -25,9 +35,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 56,
+    marginHorizontal: '3%',
   },
-  bookTile:{
-    marginHorizontal: '2%',
+  head:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '10%',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+  icon:{
+    height: 40,
+    width: 40,
+  },
+  shop:{
+    flexDirection: 'row',
+  },
+  shopCount:{
+    marginTop: '55%',
   },
   bookTitle:{
     fontSize: 20,
